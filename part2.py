bda7bbf97d86fdc44a991dad93e6766570885ea5
Diya Mangaraj <94643813+alyoshasimp@users.noreply.github.com>
2021-11-30 16:51:37 +0000

    Part 1: repeated with third, fourth and fifth
    Part 2: Added int()
    
    Part 3: added () to first print, added "" to the second, fixed multiplication formatting,  added an int to num1, moved a comma outside the quotations for second print, made number2 lowercase in first print, added a ) to number 2-- maybe my own mistake
    
    Part 4: wrote the code
    
    Part 5: wrote the code

--- part2.py
+++ part2.py
@@ -5,11 +5,13 @@ ______
 
 Run the code below. Fix it so that the program will print the product of the number 
 and 10 in the formatted sentence that's already there, and the sum of 99 and the 
-number in the formatted sentence that's already there.
+number in the formatted sentence that's a2lready there.
 
 '''
 
 #code starts here
-number = input("Enter a number: ")
+number = int(input("Enter a number: "))
 print("Ten times your number is", 10 * number)
 print("Ninety-nine more than your number is", 99 + number)
+
+#added the int
--- part3.py
+++ part3.py
@@ -7,8 +7,10 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 '''
 
 #code starts here
-number1 = input("Enter a number: ")
-number2 = int(input("Enter another number: ")
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+print("The sum of your numbers is", number1 + number2) 
+print("Seven times your second number is", 7 * number2)
+
+#added () to first print, added "" to the second, fixed multiplication formatting,  added an int to num1, moved a comma outside the quotations for second print, made number2 lowercase in first print, added a ) to number 2-- maybe my own mistake
